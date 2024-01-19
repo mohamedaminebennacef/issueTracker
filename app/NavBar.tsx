2192d3ea14ab0c7a4b361edb4267832e273682a6
mohamed bennacef <aminebennacef098@gmail.com>
2024-01-19 17:22:36 +0100

    style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,24 +1,39 @@
+'use client' // because the hook is dependant on browser api
+
 import React from 'react'
-import { IoBugSharp } from "react-icons/io5";
 import Link from 'next/link'
-
+import { IoBugSharp } from "react-icons/io5";
+import { usePathname } from 'next/navigation'; // hook
+import classnames from 'classnames'
 const NavBar = () => {
+    const currentPath = usePathname(); // / /issues
     const links = [
-        {label : 'Dashboard',href : '/'},
-        {label : 'Issues',href : '/issues'},
+        { label: 'Dashboard', href: '/' },
+        { label: 'Issues', href: '/issues' },
     ]
-  return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-7 items-center'>
-       <Link href="/"><IoBugSharp /></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link =>
-                <Link 
-                    key = {link.href}
-                    className='text-zinc-500 hover:text-zinc-800 transition-colors' 
-                    href={link.href}>{link.label}</Link> )}
-        </ul>
-    </nav>
-  )
-}
+    return (
+        <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+            <Link href="/">
+                <IoBugSharp />
+            </Link>
+            <ul className='flex space-x-6'>
+                {
+                    links.map(link =>
+                        <Link
+                            key={link.href}
+                            //className={`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`}
+                            className = {classnames({
+                                'text-zinc-900' : link.href === currentPath,
+                                'text-zinc-500' : link.href != currentPath,
+                                'hover:text-zinc-800 transition-colors':true // render this class all time
 
-export default NavBar
+                            })}
+                            href={link.href}> {link.label}
+                        </Link>
+                    )
+                }
+            </ul>
+        </nav>
+    )
+}
+export default NavBar   

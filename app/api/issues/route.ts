3dd14a27b8fdf7c2509a790939a326e649aaf86f
mohamed bennacef <aminebennacef098@gmail.com>
2024-01-21 11:50:32 +0100

    Handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import {z} from 'zod'
 import prisma from '@/prisma/client';
 
 const createIssueSchema = z.object({
-    title : z.string().min(1).max(255),
-    description : z.string().min(1)
+    title : z.string().min(1,"title is required").max(255),
+    description : z.string().min(1,"description is required")
 }); 
 
 export async function POST(request:NextRequest) {
@@ -12,9 +12,9 @@ export async function POST(request:NextRequest) {
     const validation = createIssueSchema.safeParse(body);
     
     if (!validation.success) 
-        return NextResponse.json(validation.error.errors,{ status : 400});
+        return NextResponse.json(validation.error.format(),{ status : 400});
     
-        const newIssue = await prisma.issue.create({
+         const newIssue = await prisma.issue.create({
         data : {title : body.title, description : body.description}
     })
     return NextResponse.json(newIssue,{status : 201}) 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -5,6 +5,10 @@ import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form'
 import axios from 'axios'
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Callout} from '@radix-ui/themes';
+
+
 
 interface IssueForm {
     title: string;
@@ -13,26 +17,38 @@ interface IssueForm {
 
 const NewIssuePage = () => {
     const router = useRouter();
+    const [error, setErr] = useState('');
     const { register, control, handleSubmit } = useForm<IssueForm>();
     return (
-        <form className='max-w-xl space-y-3'
-            onSubmit=
-            {
-                handleSubmit(async (data) => {
-                    await axios.post('/api/issues', data);
-                    router.push('/issues');
-                })
-            }>
-            <TextField.Root>
-                <TextField.Input placeholder="Title" {...register("title")} />
-            </TextField.Root>
-            <Controller
-                name="description"
-                control={control}
-                render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
-            />
-            <Button>Submit New Issue</Button>
-        </form>
+        <div className='max-w-xl'>
+            {error &&
+                <Callout.Root color="red" className='mb-5'>
+                    <Callout.Text> {error}</Callout.Text>
+                </Callout.Root>
+            }
+            <form className='space-y-3'
+                onSubmit=
+                {
+                    handleSubmit(async (data) => {
+                        try {
+                            await axios.post('/api/issues', data);
+                            router.push('/issues');
+                        } catch (error) {
+                            setErr('Please fill out the Title and Description field')
+                        }
+                    })
+                }>
+                <TextField.Root>
+                    <TextField.Input placeholder="Title" {...register("title")} />
+                </TextField.Root>
+                <Controller
+                    name="description"
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+                />
+                <Button>Submit New Issue</Button>
+            </form>
+        </div>
     )
 }
 export default NewIssuePage

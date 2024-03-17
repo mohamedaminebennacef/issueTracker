147ac648f7b504f81902494e0b834348db4b46ba
mohamed bennacef <aminebennacef098@gmail.com>
2024-03-17 10:49:26 +0100

    Build an API for updating issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -7,7 +7,7 @@ import { z } from 'zod';
 
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
-import { createIssueSchema } from '@/app/validationSchema';
+import { issueSchema } from '@/app/validationSchema';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import "easymde/dist/easymde.min.css";
@@ -19,14 +19,14 @@ const SimpleMDE = dynamic(
     () => import('react-simplemde-editor'),
     { ssr : false} // tell next js to not render this component on the server ssr(server side rendering)
     )
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 const IssueForm = ({issue} : {issue?: Issue}) => {
     const router = useRouter();
     const [error, setErr] = useState('');
     const [isSubmitting, setSubmitting] = useState(false);
     const { register, control, handleSubmit, formState: { errors } } = useForm<IssueFormData>({
-        resolver: zodResolver(createIssueSchema)
+        resolver: zodResolver(issueSchema)
     });
     const onSubmit = handleSubmit(async (data) => {
         try {

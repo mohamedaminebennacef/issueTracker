2f6de11574f1f668b35d646e98a41a04532935c4
mohamed bennacef <aminebennacef098@gmail.com>
2024-03-18 17:56:59 +0100

    Add the Prisma adapter

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/prisma/client';
-import { issueSchema } from '../../validationSchema';
+import { issueSchema } from '../validationSchema';
 
 export async function POST(request:NextRequest) {
     const body = await request.json();

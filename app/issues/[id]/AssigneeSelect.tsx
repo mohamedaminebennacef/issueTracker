95f18bb225ca287586ad6594bc0b2598c5de3b4e
mohamed bennacef <aminebennacef098@gmail.com>
2024-03-20 09:21:18 +0100

    Assign an Issue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -3,11 +3,11 @@
 import { Select } from '@radix-ui/themes'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import {User} from '@prisma/client';
+import {Issue, User} from '@prisma/client';
 import { useQuery } from '@tanstack/react-query';
 import {Skeleton} from '@/app/components'
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({issue} : {issue : Issue}) => {
     const {data : users,error,isLoading} = useQuery<User[]>({
         queryKey : ['users'],
         queryFn : () => axios.get('/api/users').then(res => res.data),
@@ -17,11 +17,14 @@ const AssigneeSelect = () => {
     if (isLoading) return <Skeleton/>
     if (error) return null;
     return (
-        <Select.Root>
+        <Select.Root defaultValue = {issue.assignedToUserId || ""}onValueChange={(userId) => {
+            axios.patch('/api/issues/'+issue.id,{assignedToUserId : userId || null})
+        }}>
             <Select.Trigger placeholder='Assign...' />
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
+                    <Select.Item value="">Unassigned</Select.Item>
                     {users?.map(user =>
                         (<Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)
                     )
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const issueSchema = z.object({
-    title: z.string().min(1, "Title is required").max(65535).max(255),
-    description: z.string().min(1, "Description is required")
+    title: z.string().min(1, "Title is required").max(255),
+    description: z.string().min(1, "Description is required").max(65535)
 });
 export const patchIssueSchema = z.object({
     title: z.string().min(1, "Title is required").max(255).max(65535).optional(),
